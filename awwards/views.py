e735fee6b046d115a030cee0eda3687e6911bf24
Preston-Too <toopreston@gmail.com>
2021-01-24 00:35:39 +0300

    added views function home

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    projects = Projects.objects.all()
+    context = {
+    "projects":projects,
+    }
+    return render(request, 'index.html', locals())
