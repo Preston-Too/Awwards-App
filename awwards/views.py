69780ee0a38e1b0664e15e4284a857e7120fe051
Preston-Too <toopreston@gmail.com>
2021-01-24 00:37:35 +0300

    added views function registration

--- awwards/views.py
+++ awwards/views.py
@@ -10,3 +10,17 @@ def home(request):
     "projects":projects,
     }
     return render(request, 'index.html', locals())
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'registration/register.html', context)
