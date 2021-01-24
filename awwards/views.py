87db8e3baa76fe4a6abb6fa5d2c8d1322ba00f6e
Preston-Too <toopreston@gmail.com>
2021-01-24 11:43:45 +0300

    added views function get_project

--- awwards/views.py
+++ awwards/views.py
@@ -94,3 +94,9 @@ def postproject(request):
         'form':form,
     }
     return render(request, 'PostProject.html', context)
+
+@login_required(login_url='/accounts/login/')
+def get_project(request, id):
+    project = Projects.objects.get(pk=id)
+
+    return render(request, 'project.html', {'project':project})
